a4bb188421db1eeb078cfdea62f8585fdb4ea273
Gyöngyösi <gyongyosi.andras@marktsoft.hu>
2023-07-10 13:11:06 +0200

    initial commit

---
+++ src/test/java/hu/marktsoft/epipoc/EpipocApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.marktsoft.epipoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpipocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
